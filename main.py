e5805ed1154bb0b827d16a41202faf3be6ed73b3
Morrison Winterhalder <morrisonwinterhalder@Morrisons-MacBook-Pro.local>
2026-07-05 21:54:43 -0400

    Initial project structure

---
+++ main.py
@@ -0,0 +1,6 @@
+from objects.sequences import square_numbers
+
+sequence = square_numbers(10)
+
+print("Generated sequence:")
+print(sequence)
---
+++ objects/sequences.py
@@ -0,0 +1,3 @@
+def square_numbers(n):
+    """Return the first n square numbers."""
+    return [i**2 for i in range(1, n + 1)]
